06381eb4a9f1d956670ccbe89fe88125b0efee85
Jonathon Halliwell <jon@bantros.net>
2026-07-25 12:35:35 +0100

    Add shadcn/ui accordion and button components

---
+++ src/components/ui/accordion.tsx
@@ -0,0 +1,83 @@
+import { Accordion as AccordionPrimitive } from '@base-ui/react/accordion';
+import { Minus, Plus } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+function Accordion({ className, ...props }: AccordionPrimitive.Root.Props) {
+  return (
+    <AccordionPrimitive.Root
+      data-slot='accordion'
+      className={cn('flex w-full flex-col gap-y-1', className)}
+      {...props}
+    />
+  );
+}
+
+function AccordionItem({ className, ...props }: AccordionPrimitive.Item.Props) {
+  return (
+    <AccordionPrimitive.Item
+      data-slot='accordion-item'
+      className={cn('grid gap-y-1', className)}
+      {...props}
+    />
+  );
+}
+
+function AccordionTrigger({
+  className,
+  children,
+  ...props
+}: AccordionPrimitive.Trigger.Props) {
+  return (
+    <AccordionPrimitive.Header className='flex'>
+      <AccordionPrimitive.Trigger
+        data-slot='accordion-trigger'
+        // className={cn(
+        //   'focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 aria-disabled:pointer-events-none aria-disabled:opacity-50',
+        //   className
+        // )}
+        className={cn(
+          'group/accordion-trigger relative grid grid-cols-[3fr_6fr_0.5fr] gap-4 items-center w-full h-15 pl-6 pr-2 text-xl text-left bg-gray-100 rounded-4xl outline-none cursor-pointer transition-all data-panel-open:opacity-50',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        <div className='flex items-center justify-center size-11 bg-white rounded-full'>
+          <Plus
+            data-slot='accordion-trigger-icon'
+            className='pointer-events-none shrink-0 group-aria-expanded/accordion-trigger:hidden'
+          />
+          <Minus
+            data-slot='accordion-trigger-icon'
+            className='pointer-events-none hidden shrink-0 group-aria-expanded/accordion-trigger:inline'
+          />
+        </div>
+      </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+  );
+}
+
+function AccordionContent({
+  className,
+  children,
+  ...props
+}: AccordionPrimitive.Panel.Props) {
+  return (
+    <AccordionPrimitive.Panel
+      data-slot='accordion-content'
+      className='overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up'
+      {...props}
+    >
+      <div
+        className={cn(
+          'h-(--accordion-panel-height) pt-0 pb-10 data-ending-style:h-0 data-starting-style:h-0',
+          className
+        )}
+      >
+        {children}
+      </div>
+    </AccordionPrimitive.Panel>
+  );
+}
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,50 @@
+import { Button as ButtonPrimitive } from '@base-ui/react/button';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+
+const buttonVariants = cva(
+  "group/button inline-flex shrink-0 items-center justify-center text-xl whitespace-nowrap border border-transparent rounded-none bg-clip-padding transition-all outline-none select-none cursor-pointer focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-6",
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        outline:
+          'bg-transparent border-border hover:text-foreground hover:bg-muted aria-expanded:text-foreground aria-expanded:bg-muted dark:hover:bg-input/30',
+        secondary:
+          'text-secondary-foreground bg-secondary hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)]  aria-expanded:text-secondary-foreground aria-expanded:bg-secondary',
+        ghost:
+          'hover:text-foreground hover:bg-muted aria-expanded:text-foreground aria-expanded:bg-muted dark:hover:bg-muted/50',
+        destructive:
+          'text-destructive bg-destructive/10 hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40',
+        link: 'text-primary underline underline-offset-4 hover:underline'
+      },
+      size: {
+        default:
+          'h-15 gap-30 px-7 has-data-[icon=inline-start]:pl-4 has-data-[icon=inline-end]:pr-4',
+        sm: 'h-9 gap-1 px-4 has-data-[icon=inline-start]:pl-3 has-data-[icon=inline-end]:pr-3',
+        lg: 'h-11 gap-1.5 px-8 has-data-[icon=inline-start]:pl-5 has-data-[icon=inline-end]:pr-5'
+      }
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default'
+    }
+  }
+);
+
+function Button({
+  className,
+  variant = 'default',
+  size = 'default',
+  ...props
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+  return (
+    <ButtonPrimitive
+      data-slot='button'
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
